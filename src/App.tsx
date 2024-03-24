ef14202cf97628e5fc4bcbac04064a6c193f1519
mashkn <mariya.novokhatska@gmail.com>
2024-03-23 17:37:34 -0700

    finished routing to page 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./App.scss"
-import Header from "./components/Header";
 import Main from "./pages/Main";
 import SiteList from "./pages/SiteList";
+import { RouterProvider as Router, createMemoryRouter } from 'react-router-dom';
+
+
+// const App: React.FC = () => {
+//   return (
+//     <Main/>
+//   );
+// };
+// export default App;
+const pages = createMemoryRouter([
+  {
+    path: '/home',
+    element: <Main />,
+  },
+  {
+    path: '/site-list',
+    element: <SiteList/>,
+  }
+],{initialEntries: ["/home", "/site-list"],
+initialIndex: 0,});
+
+// const App: React.FC = () => {
+//   const [currentPage, setCurrentPage] = useState<string>('home'); // Initial page
+//   console.log(currentPage);
+  // useEffect(() => {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs:any) => {
+  //     if (tabs[0].url?.includes('site-list')) {
+  //       setCurrentPage('site-list');
+  //     } else {
+  //       setCurrentPage('home');
+  //     }
+  //   });
+  // }, []);
+
+//   const renderContent = () => {
+//     switch (currentPage) {
+//       case 'home':
+//         return <Main />;
+//       case 'site-list':
+//         return <SiteList 
+//         // data={fetchDataForSavedWebsites()} 
+//         />;
+//       default:
+//         return <div>Invalid Page</div>;
+//     }
+//   };
+
+//   return renderContent();
+
+// };
+// export default App;
 
 
 const App: React.FC = () => {
   return (
-    <Main/>
+    <React.StrictMode>
+        <Router router={pages}/>
+    </React.StrictMode>
   );
 };
+
 export default App;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,17 +2,24 @@ import './Main.scss';
 import Header from "../components/Header";
 import { FaCircle } from "react-icons/fa";
 import { FaPlay } from "react-icons/fa6";
+import SiteList from './SiteList';
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+// import {broswerHistory} from 'react-router-dom';
 
 const Main = () => {
+  // const [currentPage, setCurrentPage] = useState<string>('home');
+  // const switchPage = () => browserHistory.push('site-list');
+  const navigate = useNavigate();
   return (
     <div>
       <Header page="home"/>
       <div className="Main">
     
-      <h3>Dim this page <FaCircle /></h3>
+      <h3 >Dim this page <FaCircle /></h3>
       <hr/>
       <p>Always dim this page</p>
-      <h3>Saved pages list <FaPlay/></h3>
+      <button onClick={() => navigate('/site-list')}><h3>Saved pages list <FaPlay/></h3></button>
       <hr/>
       <div id="signature">
       <p >created by mashkn</p>
